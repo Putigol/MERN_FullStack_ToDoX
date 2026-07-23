db7bc70407cbc84d31aea61d54ba27cf4fd66dcd
Nguyen Phuc Thinh <23521504@gm.uit.edu.vn>
2026-07-23 09:21:55 +0700

    Logic Controllers & Test API with Thunder Client

--- backend/src/controllers/tasksController.js
+++ backend/src/controllers/tasksController.js
@@ -1,17 +1,61 @@
-export const getAllTasks = (req, res) => {
-  res.status(200).send("Bạn có 21 việc cần làm");
+import Task from "../models/Task.js";
+
+export const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find().sort({ createdAt: -1 }); //sắp xếp theo task mới nhất
+    res.status(200).json(tasks);
+  } catch (error) {
+    console.error("Lỗi khi gọi getAllTasks:", error);
+    res.status(500).json({ message: "Lỗi hệ thống !" });
+  }
 };
 
-export const createTask = (req, res) => {
-  res
-    .status(201)
-    .json({ message: "Nhiệm vụ mới đã được thêm vào thành công!" });
+export const createTask = async (req, res) => {
+  try {
+    const { title } = req.body;
+    const task = new Task({ title }); //tạo task
+    const newTask = await task.save(); //lưu vào database
+    res.status(201).json(newTask);
+  } catch (error) {
+    console.error("Lỗi khi gọi createTask:", error);
+    res.status(500).json({ message: "Lỗi hệ thống !" });
+  }
 };
 
-export const updateTask = (req, res) => {
-  res.status(200).json({ message: "Nhiệm vụ được cập nhật!" });
+export const updateTask = async (req, res) => {
+  try {
+    const { title, status, completedAt } = req.body;
+    //tạo task để dễ test API
+    const updatedTask = await Task.findByIdAndUpdate(
+      req.params.id, //lấy id từ URL
+      {
+        title,
+        status,
+        completedAt,
+      },
+      { new: true }, //Trả về value sau khi update
+    );
+
+    if (!updatedTask) {
+      return res.status(404).json({ message: "Nhiệm vụ không tồn tại!" });
+    }
+
+    res.status(200).json(updatedTask);
+  } catch (error) {
+    console.error("Lỗi khi gọi updateTask:", error);
+    res.status(500).json({ message: "Lỗi hệ thống !" });
+  }
 };
 
-export const deleteTask = (req, res) => {
-  res.status(200).json({ message: "Nhiệm vụ được xoá!" });
+export const deleteTask = async (req, res) => {
+  try {
+    const deletedTask = await Task.findByIdAndDelete(req.params.id);
+    if (!deletedTask) {
+      return res.status(404).json({ message: "Nhiệm vụ không tồn tại!" });
+    }
+    res.status(200).json({ message: "Nhiệm vụ được xoá!" });
+  } catch (error) {
+    console.error("Lỗi khi gọi deleteTask:", error);
+    res.status(500).json({ message: "Lỗi hệ thống !" });
+  }
 };
--- backend/src/server.js
+++ backend/src/server.js
@@ -4,17 +4,21 @@ import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 
 dotenv.config();
+
 const PORT = process.env.PORT || 5001;
 
 //Tạo 1 app
 const app = express();
 
-connectDB();
+app.use(express.json()); //middleware
 
 //Khai báo dùng API
 app.use("/api/tasks", taskRoute);
 
-//lắng nghe
-app.listen(PORT, () => {
-  console.log("Server is running on port 5001");
+//Kết nối db xong thì server mới thực sự chạy
+connectDB().then(() => {
+  //lắng nghe
+  app.listen(PORT, () => {
+    console.log("Server is running on port 5001");
+  });
 });
