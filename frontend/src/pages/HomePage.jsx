6863a88d71b0d91a2f6342a5af8aa60fac1811c6
Nguyen Phuc Thinh <23521504@gm.uit.edu.vn>
2026-07-26 23:59:45 +0700

    Homepage's UI

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,44 @@
+import AddTask from "@/components/AddTask";
+import DateTimeFilter from "@/components/DateTimeFilter";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import StatsAndFilters from "@/components/StatsAndFilters";
+import TaskList from "@/components/TaskList";
+import TaskListPagination from "@/components/TaskListPagination";
+
 const HomePage = () => {
   return (
-    <div>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    <div className="min-h-screen w-full bg-white relative">
+      {/* Pink Glow Background */}
+      <div
+        className="absolute inset-0 z-0"
+        style={{
+          backgroundImage: `
+        radial-gradient(125% 125% at 50% 90%, #ffffff 40%, #ec4899 100%)
+      `,
+          backgroundSize: "100% 100%",
+        }}
+      />
+      {/* Your Content/Components */}
+      <div className="container pt-8 mx-auto relative z-10">
+        <div className="w-full max-w-2xl p-6 mx-auto space-y-6">
+          {/* Đầu trang */}
+          <Header />
+          {/*Tạo nhiệm vụ */}
+          <AddTask />
+          {/* Thống kê và bộ lọc */}
+          <StatsAndFilters />
+          {/* Danh sách nhiệm vụ */}
+          <TaskList />
+          {/* Phân trang và Lọc theo date */}
+          <div className="flex flex-col items-center justify-center gap-6 sm:flex-row">
+            <TaskListPagination />
+            <DateTimeFilter />
+          </div>
+          {/* Chân trang */}
+          <Footer />
+        </div>
+      </div>
     </div>
   );
 };
