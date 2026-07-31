eced93cc9d4ad54014cc40ae3eb81d0c2dbdead7
Nguyen Phuc Thinh <23521504@gm.uit.edu.vn>
2026-07-31 23:41:55 +0700

    Homepage's UI: Footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,5 +1,25 @@
-const Footer = () => {
-  return <div>Footer</div>;
+const Footer = ({ completedTasksCount = 0, activeTasksCount = 0 }) => {
+  return (
+    <>
+      {completedTasksCount + activeTasksCount > 0 && (
+        <div className="text-center">
+          <p className="text-sm text-muted-foreground">
+            {completedTasksCount > 0 && (
+              <>
+                🎉 Tuyệt vời! Bạn đã hoàn thành {completedTasksCount} việc
+                {activeTasksCount > 0 &&
+                  `, còn ${activeTasksCount} việc nữa thôi. Cố lên!`}
+              </>
+            )}
+
+            {completedTasksCount === 0 && activeTasksCount > 0 && (
+              <>Bạn còn {activeTasksCount} việc chưa hoàn thành. Cố lên!</>
+            )}
+          </p>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Footer;
